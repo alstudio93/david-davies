8794a2e0e1301e7d73df501d155d9ea44baf4dd2
Ladd1993 <aladd1993@gmail.com>
2022-08-29 18:18:13 -0500

    allowFullScreen

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6530.913486163576!2d-92.453114!3d35.070319!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87d282708a8afd87%3A0xbe2799bf07520db4!2s1972%20Dave%20Ward%20Dr%2C%20Conway%2C%20AR%2072034!5e0!3m2!1sen!2sus!4v1661812927858!5m2!1sen!2sus"
                         width="250"
                         height="200"
-                        allowfullscreen=""
+                        allowFullScreen=""
                         loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"
                         className='rounded-lg' />
